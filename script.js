5efe632df710298daa936bc91e8a274aa4b1a495
devhomie <devhomie@proton.me>
2024-07-01 09:24:23 -0400

    updated the -envelope- property of -Tone.Synth- instance wrapped in -synth- variable
    the second argument to -triggerAttackRelease- is the duration of the note in seconds, and the third
    argument is the time at which to play the note, also in seconds. -- The first argument is the note
    to be played. Starting from A3 -> B3 -> C#4 -> ... A4
    
    Each note will play for 1 second total in sequence with no overlap between notes

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ Tone.start();
 
 let synth = new Tone.Synth({
   oscillator: { type: "square"},
-  envelope: { attack: 0.8, decay: 0.3, sustain: 0.8, release: 1},
-  volume: -6
+  envelope: { attack: 0.1, decay: 0.3, sustain: 0.8, release: 1},
+  volume: -24
 }).toDestination();
 
-synth.triggerAttackRelease("A4", 2, 0)
+synth.triggerAttackRelease("A3", 0.9, 0);
+synth.triggerAttackRelease("B3", 0.9, 1);
+synth.triggerAttackRelease("C#4", 0.9, 2);
+synth.triggerAttackRelease("D4", 0.9, 3);
+synth.triggerAttackRelease("E4", 0.9, 4);
+synth.triggerAttackRelease("F#4", 0.9, 5);
+synth.triggerAttackRelease("G#4", 0.9, 6);
+synth.triggerAttackRelease("A4", 0.9, 7);
 
 
 });
