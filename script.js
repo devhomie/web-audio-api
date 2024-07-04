9dc5fcfc187128d25b94324a303475306db54b2b
devhomie <devhomie@proton.me>
2024-07-04 12:59:39 -0400

    replaced -Tone.Synth- object with -Tone.PolySynth-
    The -Tone.PolySynth- constructor takes two arguments: a monophonic synth (in this case, -Tone.Synth-)
    and an object with the options that would normally be passed to that monophonic synth's constructor.
    
    The polysynth then screates multiple monophonic synths with the specified settings, effectively allowing
    it to play multiple notes at once.
    
    Next, we play the same scale but with addtional simultaneous notes. This is achieved by passing an array of
    note names to the -tiggerAttackRelease- method instead of a single note name.
     For Example: We pass the array [A3, C#4] to play A3 and C#4 as the same time.
    
    When you play this example, you should hear a harmonized scale with a nice magor chord at the end.

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ play.addEventListener("click", () => {
 
 Tone.start();
 
-let synth = new Tone.Synth({
+let synth = new Tone.PolySynth(Tone.Synth,{
   oscillator: { type: "square"},
-  envelope: { attack: 0.1, decay: 0.3, sustain: 0.8, release: 1},
-  volume: -24
+  envelope: { attack: 0.1, decay: 0.3, sustain: 0.8, release: 0.1},
+  volume: -6
 }).toDestination();
 
-synth.triggerAttackRelease("A3", 0.9, 0);
-synth.triggerAttackRelease("B3", 0.9, 1);
-synth.triggerAttackRelease("C#4", 0.9, 2);
-synth.triggerAttackRelease("D4", 0.9, 3);
-synth.triggerAttackRelease("E4", 0.9, 4);
-synth.triggerAttackRelease("F#4", 0.9, 5);
-synth.triggerAttackRelease("G#4", 0.9, 6);
-synth.triggerAttackRelease("A4", 0.9, 7);
+synth.triggerAttackRelease(["A3", "C#4"], 0.9, 0);
+synth.triggerAttackRelease(["B3", "D4"], 0.9, 1);
+synth.triggerAttackRelease(["C#4", "E#4"], 0.9, 2);
+synth.triggerAttackRelease(["D4", "F#4"], 0.9, 3);
+synth.triggerAttackRelease(["E4", "G#4"], 0.9, 4);
+synth.triggerAttackRelease(["F#4", "A4"], 0.9, 5);
+synth.triggerAttackRelease(["G#4", "B4"], 0.9, 6);
+synth.triggerAttackRelease(["A4", "A4"], 0.9, 7);
 
 
 });
