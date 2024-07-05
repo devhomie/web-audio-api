93875e08a17b178ba054ff5e9ed1ae8dbcafdc85
devhomie <devhomie@proton.me>
2024-07-04 22:37:54 -0400

    changed the -synth- and created a -Tone.Part- object
    The first change we're making here is to the synth. Using a polyphonic synth, so we can play multiple notes at
    the same time. -- Other than the synth being different, the body of the callback function is the same. We're
    still calling -synth.triggerAttackRelease- and passing the _note_ and _time_ parameters, -- Which -Tone.Part-
    will fill in automatically.
    
    Next comes the array of time/note pairs. Notice that some of the notes are arrays themselves; This pair of notes
    will be passed to the -triggerAttackRelease- method unchanged and will have the effect of play two notes at once,
    
    Finally, we call -.start('0:0:0')- which has the effect of playing this part immediately. If we used -.start('1:0:0')
    instead, for example, then the melody would start after a bar's pause.
    
    The times given for each time/note pair are relative to the time passed to the start method.
    
    When you play this example, you should hear the beginning of 'Mary Had a Little Lamb'

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ play.addEventListener("click", () => {
 
 Tone.start();
 
-let synth = new Tone.Synth().toDestination();
+let synth = new Tone.PolySynth(Tone.Synth).toDestination();
 
-new Tone.Sequence((time, note) =>{
+new Tone.Part((time, note) => {
   synth.triggerAttackRelease(note, "16n", time);
-}, [ "C4", null, "B3", "C4", "G3", "A3", null, "B3"], "8n").start("0:0:0").stop("4.0.0");
+}, [
+  ["0:0:0", ["C3", "E4"]],
+  ["0:0:3", "D4"],
+  ["0:1:0", "C4"],
+  ["0:1:2", "D4"],
+  ["0:2:0",["E3", "E4"]],
+  ["0:2:2", "E4"],
+  ["0:3:0", "E4"],
+  ["1:0:0", ["G3", "D4"]],
+  ["1:0:2", "D4"],
+  ["1:1:0", "D4"],
+  ["1:1:0", "D4"],
+  ["1:2:0", ["E3", "E4"]],
+  ["1:1:2", "G4"],
+  ["1:3:0", "G4"]
+]).start("0:0:0");
 
 Tone.Transport.start();
 });
